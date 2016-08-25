2bf3a894b52c42615daf394ae84b51e6d8a3943a
Jogyn <digitalwave@protonmail.ch>
2016-08-25 12:49:14 -0700

    autocomplete in mainJS

---
+++ myflaskapp/static/js/autocomplete.js
@@ -0,0 +1,52 @@
+var updateAddress = {
+    autocomplete: new google.maps.places.Autocomplete($("#address")[0], {
+        types: ['geocode'],
+        componentRestrictions: {country: 'ca'}
+    }),
+    event: function(){
+        var self = this;
+        google.maps.event.addListener(self.autocomplete, 'place_changed', function() {
+            var place = self.autocomplete.getPlace(),
+                address = place.address_components,
+                streetAddress = '',
+                city = '',
+                state = '',
+                zip = '',
+                country = '';
+
+            for (var i = 0; i < address.length; i++) {
+                var addressType = address[i].types[0];
+
+                if (addressType == 'subpremise') {
+                    streetAddress += address[i].long_name + '/';
+                }
+                if (addressType == 'street_number') {
+                    streetAddress += address[i].long_name + ' ';
+                }
+                if (address[i].types[0] == 'route') {
+                    streetAddress += address[i].long_name;
+                }
+                if (addressType == 'locality') {
+                    suburb = address[i].long_name;
+                }
+                if (addressType == 'administrative_area_level_1') {
+                    state = address[i].long_name;
+                }
+                if (addressType == 'postal_code') {
+                    zip = address[i].long_name;
+                }
+                if (addressType == 'country') {
+                    country = address[i].long_name;
+                }
+            }
+
+            // update the textboxes
+            setTimeout(function(){$('#address').val(streetAddress);},50);
+            $('#city').val(suburb);
+            $('#state').val(state);
+        });
+
+    }
+};
+
+updateAddress.event();
